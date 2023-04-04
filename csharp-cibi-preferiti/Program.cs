7322b7794205dedf73cab05fc7a787e05e7aedff
slivcaigor <slivcaigor@gmail.com>
2023-04-04 14:15:48 +0200

    Inizializzazione e Stampa Lunghezza

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+// Nel programma inizializzate un array con la classifica dei vostri cibi preferiti (minimo 5, massimo 10 elementi). L’array deve essere già inizializzato nel programma, e i vostri cibi preferiti non vanno chiesti all’utente tramite input.
+
+
+//Una volta dichiarato l’array di cibi preferiti, stampate a video le seguenti informazioni:
+//-La lunghezza della classifica
+//- La vostra classifica (dunque stampare l’intero array in ordine indicando la posizione in classifica)
+//-Il vostro cibo top (prima posizione della classifica)
+//-Il vostro cibo preferito ma non troppo (ultima posizione della classifica)
+
+
+//Stampate a video anche il cibo di mezza classifica, cioè che si trova nella posizione **mediana.** Attenzione: gestire anche il caso se aveste una classifica con un numero di elementi pari. In questo caso vanno stampati i 2 elementi in centro alla vostra classifica.
+
+
+// Inizializzazione dell'array di cibi preferiti
+string[] cibiPreferiti = { "Sushi", "Pizza", "Pokè", "Spaghetti", "Risotto", "Tacos" };
+
+// Stampa la lunghezza dell'array di cibi preferiti
+Console.WriteLine($"La classifica è composta da {cibiPreferiti.Length} piatti");
